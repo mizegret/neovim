b2b6933537c67d958cc86eee41019eff0458379d
mizegret <mizegret@example.com>
2026-01-31 15:29:52 +0900

    feat: add core options

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -1,6 +1,14 @@
 vim.g.mapleader = " "
 
+vim.opt.termguicolors = true
+
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+
+-- transparent background: https://github.com/basecamp/omakub/issues/64
+vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
