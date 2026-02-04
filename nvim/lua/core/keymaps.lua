14bf6994303a6bc2452e8a47c5564a814d9321a1
mizegret <mizegret@example.com>
2026-02-04 23:03:41 +0900

    feat: add keybind for diffview.

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,1 +1,14 @@
 vim.keymap.set("i", "jj", "<Esc>", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<leader>do", "<cmd>DiffviewOpen<CR>", { desc = "Diffview open" })
+vim.keymap.set("n", "<leader>dc", "<cmd>DiffviewClose<CR>", { desc = "Diffview close" })
+vim.keymap.set("n", "<leader>dh", "<cmd>DiffviewFileHistory<CR>", { desc = "Diffview history" })
+vim.keymap.set("n", "<leader>dH", "<cmd>DiffviewFileHistory %<CR>", { desc = "History current file" })
+vim.keymap.set("n", "<leader>dd", function()
+  local view = require("diffview.lib").get_current_view()
+  if view then
+    vim.cmd("DiffviewClose")
+  else
+    vim.cmd("DiffviewOpen")
+  end
+end, { desc = "Diffview toggle" })
