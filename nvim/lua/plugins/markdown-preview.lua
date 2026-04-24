4d7934599e7b5c09f8419e3846266bd907765605
mizegret <mizegret@gmail.com>
2026-04-24 16:24:37 +0900

    feat: apply setting for new mac pc.

--- nvim/lua/plugins/markdown-preview.lua
+++ nvim/lua/plugins/markdown-preview.lua
@@ -1,4 +1,8 @@
-vim.g.mkdp_browser = "chrome.exe"
+if vim.fn.has("mac") == 1 then
+  vim.g.mkdp_browser = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+elseif vim.fn.has("win32") == 1 then
+  vim.g.mkdp_browser = "chrome.exe"
+end
 
 vim.pack.add({
   { src = "https://github.com/iamcco/markdown-preview.nvim" },
